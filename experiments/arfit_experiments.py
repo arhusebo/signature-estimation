854d8447a0c22592ed7ee0fdd27ba8eebf7df72a
Arild Bergesen Husebø <arhusebo@gmail.com>
2023-01-25 13:30:58 +0100

    update path and remove duplicate lines in ar fitting experiments

--- experiments/arfit_experiments.py
+++ experiments/arfit_experiments.py
@@ -27,8 +27,9 @@ class ExperimentSet(gsim.AbstractExperimentSet):
     def experiment_1001(l_args):
         print("Fitting AR model to data from UIA")
         from data.uia import UiADataLoader
+        from data import uia_path
         nsampfit = 10000 # number of samples to use for fitting model
-        dl = UiADataLoader("../andreas_data/ALT Bearing 2 h5/")
+        dl = UiADataLoader(uia_path)
         sigfit = dl["y2016-m09-d20/00-13-28 1000rpm - 51200Hz - 100LOR.h5"].vib[:nsampfit]
         sigval = dl["y2016-m09-d20/00-23-37 1000rpm - 51200Hz - 100LOR.h5"].vib[:nsampfit]
 
@@ -39,12 +40,12 @@ class ExperimentSet(gsim.AbstractExperimentSet):
     def experiment_1002(l_args):
         print("Fitting AR model to data from UNSW")
         from data.unsw import UNSWDataLoader
+        from data import unsw_path
         nsampfit = 10000 # number of samples to use for fitting model
-        dl = UNSWDataLoader("../unsw_dataset/Test 1/")
-        sigfit = dl["6Hz/vib_000002663_06.mat"].vib[:nsampfit]
-        sigval = dl["6Hz/vib_000005667_06.mat"].vib[:nsampfit]
+        dl = UNSWDataLoader(unsw_path)
+        sigfit = dl["Test 1/6Hz/vib_000002663_06.mat"].vib[:nsampfit]
+        sigval = dl["Test 1/6Hz/vib_000005667_06.mat"].vib[:nsampfit]
 
-        prange = np.arange(1, 300)
         prange = np.arange(1, 300)
         G = general_fitting_experiment(sigfit, sigval, prange)
         return G
@@ -52,12 +53,12 @@ class ExperimentSet(gsim.AbstractExperimentSet):
     def experiment_1003(l_args):
         print("Fitting AR model to data from CWRU")
         from data.cwru import CWRUDataLoader
+        from data import cwru_path
         nsampfit = 10000 # number of samples to use for fitting model
-        dl = CWRUDataLoader("../case_dataset/info.json")
+        dl = CWRUDataLoader(cwru_path)
         sigfit = dl[100].vib[:nsampfit]
         sigval = dl[100].vib[-nsampfit:]
 
-        prange = np.arange(1, 300)
         prange = np.arange(1, 300)
         G = general_fitting_experiment(sigfit, sigval, prange)
         return G
