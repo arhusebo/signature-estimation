ee49f23b3d0fc79c21c182858965b5aa7ac0c1aa
Arild Bergesen Husebø <arhusebo@gmail.com>
2024-07-28 10:01:44 +0200

    add data/__init__.py to .gitignore

--- data/__init__.py
+++
@@ -1,6 +0,0 @@
-#the following paths must be added to work with the respective datasets
-#please look at the dataloaders for more information
-
-#uia_path = "path/do/dataset"
-#unsw_path = "path/do/dataset"
-cwru_path = r"C:\Users\Arild\lagerdata\case_dataset"
